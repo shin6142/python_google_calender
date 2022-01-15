1ed7b00dce80da6fa57b390ef99d60010817933d
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-15 20:22:54 +0900

    g_calenderを追加

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from g_calender_api import GetEvent
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello, world'
+    obj = GetEvent()
+    result = obj.get_event()
+    return result
 
 if __name__ == '__main__':
     app.run()
