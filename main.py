fb419bce0986c801e201376b0e3a8d668f1d8eec
Shin Yamaga <shinyamaga@ShinnoMacBook-Air.local>
2022-01-15 20:48:43 +0900

    key取得テスト

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from test import GetEvent
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    obj = GetEvent()
-    result = obj.get_event()
-    return result
+    json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+    return json
 
 if __name__ == '__main__':
     app.run()
